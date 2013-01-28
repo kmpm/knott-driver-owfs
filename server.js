91c7713492f5ba467d651de11ba5f45a0225c873
kmpm <peter@birchroad.net>
2013-01-28 09:23:27 +0100

    logging and error management

--- server.js
+++ server.js
@@ -1,35 +1,50 @@
 
 var owfs = require('owfs'),
 	mqtt = require('mqttjs'),
-  os = require('os');
+  os = require('os'),
+  Log = require('./lib/log');
 
-var config = require('./config.json')
-var deviceinfo={};
+var config = require('./config.json'),
+    log = new Log(),
+    deviceinfo={};
+    
+log.level=config.loglevel || 3;
 
 var owclient = new owfs.Client(config.owfs.host, config.owfs.port);
 var valid_keys=['/temperature', '/PIO'];
 
 function readDevices(mqclient){
-	owclient.dirall("/", function(nodes){
-		//console.log(nodes);
-		nodes.forEach(function(nod){
-			readDevice(nod, mqclient);
-		});	
-	}); 
+  try{
+    log.info('reading devices');
+    owclient.dirall("/", function(nodes){
+      //console.log(nodes);
+      nodes.forEach(function(nod){
+        readDevice(nod, mqclient);
+      });	
+    });
+  }
+  catch (e) {
+    log.error(e); 
+  }
 }
 
 
 function readDevice(nod, mqclient){
-	console.log("processing", nod);
-	owclient.dir(nod, function(values){
-		values.forEach(function(value){
-			value = value.replace(nod, '');
-			//console.log(value);
-			if(valid_keys.indexOf(value)>=0){
-				readValue(nod, value, mqclient);
-			}
-		});
-	});
+	//console.log("processing", nod);
+  try{
+    owclient.dir(nod, function(values){
+      values.forEach(function(value){
+        value = value.replace(nod, '');
+        //console.log(value);
+        if(valid_keys.indexOf(value)>=0){
+          readValue(nod, value, mqclient);
+        }
+      });
+    });
+  }
+  catch (e) {
+    log.error(e);
+  }
 		
 }
 
@@ -48,12 +63,11 @@ function readValue(nod, value, mqclient) {
   if(! deviceinfo[nod].hasOwnProperty(value.substring(1))){
     mqclient.publish({topic:"/config/owfs/deviceinfo" + nod + value, retain:true, payload:"ro"});
   }
-  
     
   topic='/raw/' + topic;
 	//console.log("reading", vp);
 	owclient.read(vp, function(result){
-		console.log(topic, result);
+		log.debug(topic, result);
 		mqclient.publish({topic:topic, payload:result, retain:true});
 	});
 }
@@ -66,12 +80,13 @@ console.log(config);
 
 mqtt.createClient(config.mqtt.port, config.mqtt.host, function(err, client){
 	if(err){
-		console.log(err);
+		log.error(err);
 		process.exit(1);
 	}
+  log.info('knott-driver-owfs starting');
 	client.connect({keepalive:30000});
 	client.on('connack', function(packet){
-		console.log("Connected to mqtt");
+		log.info("Connected to mqtt");
     client.subscribe({topic:'/config/owfs/deviceinfo/#'});
 		//readDevices(client);
 		setInterval(readDevices, config.refresh, client );
@@ -80,20 +95,20 @@ mqtt.createClient(config.mqtt.port, config.mqtt.host, function(err, client){
 	});
 
 	client.on('close', function() {
-		console.log("mqtt connection closed");
+		log.info("mqtt connection closed");
 	});
 	client.on('error', function(e) {
-		console.log("mqtt error", e);
+		log.error("mqtt error", e);
 	});	
  
   client.on('suback', function(packet) {
-    console.log("suback", packet);
+    log.debug("suback", packet);
   });
 
   //listen for the subscribed topics so that 
   //we can get a proper alias for the device.
   client.on('publish', function(packet){
-    console.log(packet.topic, packet.payload);
+    log.debug(packet.topic, packet.payload);
     var topic = packet.topic.split('/');
     var device = '/' + topic[4];
     var key = topic[5];
@@ -102,7 +117,7 @@ mqtt.createClient(config.mqtt.port, config.mqtt.host, function(err, client){
       deviceinfo[device]={};
     }
     deviceinfo[device][key] = value;
-    console.log(key, 'set to', value, 'for device', device);
+    log.info(device, key, 'set to', value );
   });
 });
 
